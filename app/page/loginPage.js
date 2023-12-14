17a4abd06889c6852803bc3eac6042b8507cddc5
ImFaldi <rifaldi.indrajaya@binus.ac.id>
2023-12-14 18:18:58 +0700

    Register Page

--- app/page/loginPage.js
+++ app/page/loginPage.js
@@ -2,6 +2,7 @@ import { Link } from 'expo-router';
 import React from 'react';
 import { Text, View, Image, TouchableHighlight, TextInput, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { FontAwesome5 } from '@expo/vector-icons';
 export default function LoginPage() {
     return (
         <View className="flex-1 items-center bg-white mt-10">
@@ -10,23 +11,23 @@ export default function LoginPage() {
                 source={require('../../assets/img/login-page.png')}
             />
             <View className="flex flex-row items-center border mt-5 rounded-lg w-3/4 px-5 py-2">
-                <Icon name="envelope" size={20} color="gray" className="mr-2" />
-                <TextInput className="w-full"
+                <Icon name="envelope" size={20} color="gray" />
+                <TextInput className="w-full mx-2"
                     placeholder="Username"
                 />
             </View>
             <View className="flex flex-row items-center border mt-5 rounded-lg w-3/4 px-5 py-2">
-                <Icon name="lock" size={20} color="gray" className="mr-2" />
+                <Icon name="lock" size={20} color="gray" />
                 <TextInput
-                    className="w-full"
+                    className="w-full mx-2"
                     placeholder="Password"
                 />
             </View>
             <TouchableHighlight
-                className="bg-blue-500 px-5 py-3 rounded-full mt-5 w-3/4"
+                className="px-5 py-3 rounded-full mt-5 w-3/4 bg-blue-500 flex flex-row items-center justify-center"
                 onPress={() => console.log('Hello, Tailwind!')}
             >
-                <Text className="text-white font-semibold text-center text-lg">Login</Text>
+                <Text className="font-semibold text-center text-lg text-white">Login</Text>
             </TouchableHighlight>
 
             <View className="flex flex-row items-center mt-5 w-3/4">
@@ -36,28 +37,28 @@ export default function LoginPage() {
             </View>
 
             <TouchableHighlight
-                className="px-5 py-3 rounded-full mt-5 w-3/4 bg-blue-500 flex flex-row items-center justify-center"
+                className="px-5 py-2 rounded-full mt-5 w-3/4 border border-gray-500 flex flex-row items-center justify-center"
                 onPress={() => console.log('Hello, Tailwind!')}
             >
-                <Text className="font-semibold text-center text-lg text-white">Login with Facebook</Text>
+                <View className="flex flex-row items-center justify-center">
+                    <FontAwesome5 name="google" size={28} color="red" className="mx-3" />
+                    <Text className="font-semibold text-center text-lg">    Login with Google</Text>
+                </View>
             </TouchableHighlight>
 
             <TouchableHighlight
-                className="px-5 py-3 rounded-full mt-5 w-3/4 bg-red-500 flex flex-row items-center justify-center"
+                className="px-5 py-2 rounded-full mt-5 w-3/4 border border-gray-500 flex flex-row items-center justify-center"
                 onPress={() => console.log('Hello, Tailwind!')}
             >
-                <Text className="font-semibold text-center text-lg text-white">Login with Google</Text>
-            </TouchableHighlight>
-            <TouchableHighlight
-                className="px-5 py-3 rounded-full mt-5 w-3/4 bg-black flex flex-row items-center justify-center"
-                onPress={() => console.log('Hello, Tailwind!')}
-            >
-                <Text className="font-semibold text-center text-lg text-white">Login with Apple</Text>
+                <View className="flex flex-row items-center justify-center">
+                    <FontAwesome5 name="facebook" size={30} color="blue" className="mx-3" />
+                    <Text className="font-semibold text-center text-lg ">    Login with Facebook</Text>
+                </View>
             </TouchableHighlight>
 
             <View className="flex flex-row mt-5">
                 <Text className="text-gray-500">Don't have an account?</Text>
-                <Link href={'/page'} className="text-blue-500 ml-2">Sign Up</Link>
+                <Link href={'page/register/[register]'} className="text-blue-500 ml-2">Register</Link>
             </View>
         </View>
     );
---
+++ app/page/register/[register].js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+
+export default function UserPage() {
+    return (
+        <View>
+            <Text>Register Page</Text>
+        </View>
+    );
+}
